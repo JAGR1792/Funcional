5109e2dffb98c71b5968a4b7a46ae97df5b33c0f
Mariana909 <karla.duque01@usa.edu.co>
2025-05-12 18:24:35 -0500

    Create Punto_4.py

---
+++ Python/Punto_4.py
@@ -0,0 +1,6 @@
+"""Punto 4: Un maestro desea saber qué porcentaje de hombres y que porcentaje de mujeres hay en un grupo de estudiantes."""
+generos=['M','F','M','F','F','M','M','F','M','F','M','F','M','F','F','F','M','F','M','F']
+cantidad_hombres=len(list(filter(lambda elemento: elemento=='M',generos)))
+cantidad_mujeres=len(list(filter(lambda elemento: elemento=='F',generos)))
+print(f"Cantidad de hombres: {cantidad_hombres}")
+print(f"Cantidad de mujeres: {cantidad_mujeres}")
