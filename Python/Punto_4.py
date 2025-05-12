948edd644027af68ba3d88473dd77a5d35916e2b
Mariana909 <karla.duque01@usa.edu.co>
2025-05-12 18:25:22 -0500

    Update Punto_4.py

--- Python/Punto_4.py
+++ Python/Punto_4.py
@@ -1,4 +1,5 @@
 """Punto 4: Un maestro desea saber qué porcentaje de hombres y que porcentaje de mujeres hay en un grupo de estudiantes."""
+from functools import reduce
 generos=['M','F','M','F','F','M','M','F','M','F','M','F','M','F','F','F','M','F','M','F']
 cantidad_hombres=len(list(filter(lambda elemento: elemento=='M',generos)))
 cantidad_mujeres=len(list(filter(lambda elemento: elemento=='F',generos)))
