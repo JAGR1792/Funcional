b6beb17275f500c08bb10719e037aa83e8686d4a
Mariana909 <karla.duque01@usa.edu.co>
2025-05-17 18:32:18 -0500

    Update Punto_2.py

--- Python/Punto_2.py
+++ Python/Punto_2.py
@@ -1,7 +1,10 @@
-"""Punto 2: Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente desea saber cuánto deberá pagar finalmente por su compra."""
 from functools import reduce
-lista=[10000,25000,50000]
-compraTotal=reduce(lambda acumulador=0,elemento=0:acumulador+elemento,lista)
+# Lista de precios de productos del cliente
+precios=[10000,25000,50000]
+# Se calcula el total a pagar sin el descuento, sumando cada precio
+compraTotal=reduce(lambda acumulador=0,elemento=0:acumulador+elemento,precios)
+# Se calcula el valor a pagar con un descuento de 15%
 ventaDescuento=compraTotal*0.85
+# Se imprimen el valor a pagar sin descuento y con el descuento
 print(f"Valor a pagar sin descuento: {compraTotal}")
 print(f"Valor a pagar con descuento: {ventaDescuento}")
