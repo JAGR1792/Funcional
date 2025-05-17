876fa4b93ae4448180fb8a6a1caea1900135cd91
Mariana909 <karla.duque01@usa.edu.co>
2025-05-17 18:32:35 -0500

    Update Punto_3.py

--- Python/Punto_3.py
+++ Python/Punto_3.py
@@ -1,14 +1,12 @@
-"""Punto 3: Un aprendiz desea saber cuál será su calificación final en la materia de Algoritmos. Dicha calificación se compone de los siguientes porcentajes:
-
-55% del promedio de sus tres calificaciones parciales.
-
-30% de la calificación del examen final.
-
-15% de la calificación de un trabajo final."""
 from functools import reduce
-notas=[4,1,2]
+# Lista de notas del estudiante
+notas=[4,4,3]
+# Notas del examen final y trabajo final
 examen_final=5
-trabajo_final=3
+trabajo_final=4
+# Se calcula el promedio de las notas parciales del estudiantes
 notas_parciales=reduce(lambda acumulador=0,elemento=0:acumulador+elemento,notas)/len(notas)
+# Se calcula la nota final teniendo en cuenta los respectivos porcentajes
 nota_final=notas_parciales*0.55+examen_final*0.3+trabajo_final*0.15
+# Se imprime la nota final del estudiante
 print(f"Nota final: {nota_final}")
