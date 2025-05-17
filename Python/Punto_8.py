04e7bc5bd2ed4d89e8ad57fddc041c1890841a0b
Mariana909 <karla.duque01@usa.edu.co>
2025-05-17 18:34:17 -0500

    Update Punto_8.py

--- Python/Punto_8.py
+++ Python/Punto_8.py
@@ -1,10 +1,13 @@
-"""Punto 8: Realice un algoritmo que lea una secuencia de 20 números almacenarlos en un vector y mostrar la posición donde se encuentra el mayor valor leído"""
 import random
-n=int(input("Ingrese un tamaño para el arreglo: "))
+n=int(input("Ingrese un tamaño para el vector: "))
 lista=[0]*n
+"""Se recorre cada posición del arreglo con map, cambiando el elemento
+   por un número ingresado por usuario"""
 resultado=list(map(lambda elemento:int(input("Ingrese un número: ")),lista))
-print(resultado)
+# Se calculan el elemento mayor ingresado y su posicion en el vector
 maximo=max(resultado)
-print(f"Máximo: {maximo}")
 posicion=resultado.index(maximo)
+# Se imprimen la lista creada, su elemento mayor y su posición
+print(resultado)
+print(f"Máximo: {maximo}")
 print(f"Posición del máximo: {posicion}")
