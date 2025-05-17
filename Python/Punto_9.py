fc78a5070555b3a654cef7f601dadf5e3cb36235
Mariana909 <karla.duque01@usa.edu.co>
2025-05-17 18:34:37 -0500

    Update Punto_9.py

--- Python/Punto_9.py
+++ Python/Punto_9.py
@@ -1,9 +1,9 @@
-"""Punto 9: Realice un algoritmo que cree un vector de 16 posiciones llenarlo y partirlo en dos de 8 posiciones."""
 import random
 lista=[0]*16
 resultado=list(map(lambda elemento:random.randint(-100,100),lista))
-print(f"Lista Original: {resultado}")
+
 resultado1=resultado[0:8]
-print(f"Sublista 1: {resultado1}")
 resultado2=resultado[8:16]
+print(f"Lista Original: {resultado}")
+print(f"Sublista 1: {resultado1}")
 print(f"Sublista 2: {resultado2}")
