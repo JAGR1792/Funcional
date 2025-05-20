a6e5fbd501c4098bd37f3dbbfdbaf9a44db3246f
Mariana909 <karla.duque01@usa.edu.co>
2025-05-19 20:06:33 -0500

    Update Punto_1.py

--- Python/Punto_1.py
+++ Python/Punto_1.py
@@ -1,7 +1,7 @@
 from functools import reduce
 #Ventas del vendedor
-ventas=[100,200,300]
-sueldoBase=500
+ventas=list(map(lambda elemento:int(input("Ingrese su venta: ")),range(3)))
+sueldoBase=15000
 # Se suman las ventas y se obtiene el 10% para comisión
 comisiones=reduce(lambda acumulador=0,elemento=0:acumulador+elemento,ventas)*0.1
 # Se calcula el sueldo total sumando el sueldo base y las comisiones
